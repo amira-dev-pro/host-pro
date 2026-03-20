432b37f27cef611429cc0a85909f23d2c33a93a9
amira-dev-pro <amirazaki373@gmail.com>
2026-03-21 00:54:51 +0100

    fix requirements final

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI
 from routes.users import router
-
 from database import engine, Base
-from models.user import User
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Host PRO OK 🚀"}
+
+    return {"message": "Host PRO OK 🚀"} 
 
 app.include_router(router)
 
-Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine) 
